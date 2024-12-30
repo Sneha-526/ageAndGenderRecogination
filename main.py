64807c4c0743cbfc38fa25fb0cd82f5553fd3fc9
Sneha-526 <snehapandit526@gmail.com>
2024-12-30 22:00:54 +0530

    changes done

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ ageNet = cv2.dnn.readNet(ageModel, ageProto)
 genderNet = cv2.dnn.readNet(genderModel, genderProto)
 
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(21-24)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 genderList = ['Male', 'Female']
 
 video = cv2.VideoCapture(0)
