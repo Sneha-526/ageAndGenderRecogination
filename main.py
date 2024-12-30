280f26f72ed84b64150f5690833ffcbb27572d37
Sneha-526 <snehapandit526@gmail.com>
2024-12-30 22:03:57 +0530

    final changes

--- main.py
+++ main.py
@@ -70,7 +70,6 @@ def read_image(filepath):
 def index():
     return render_template('index.html')
 
-<<<<<<< HEAD
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(21-24)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 genderList = ['Male', 'Female']
